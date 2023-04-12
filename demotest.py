891fbc09ac14431471fba23420458444bca647c1
zhaoxiaozhuo1 <2577982484@qq.com>
2023-04-13 07:48:06 +0800

    testshow

---
+++ demotest.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+# markdown
+st.markdown('Streamlit Demo')
+
+# 设置网页标题
+st.title('一个傻瓜式构建可视化 web的 Python 神器 -- streamlit')
+
+# 展示一级标题
+st.header('1. 安装')
+
+st.text('和安装其他包一样，安装 streamlit 非常简单，一条命令即可')
+code1 = '''pip3 install streamlit'''
+st.code(code1, language='bash')
+
+
+# 展示一级标题
+st.header('2. 使用')
+
+# 展示二级标题
+st.subheader('2.1 生成 Markdown 文档')
+
+# 纯文本
+st.text('导入 streamlit 后，就可以直接使用 st.markdown() 初始化')
+
+# 展示代码，有高亮效果
+code2 = '''import streamlit as st
+st.markdown('Streamlit Demo')'''
+st.code(code2, language='python')
